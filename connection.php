322ab1f8f786543270ce4b21fadc36f881afb903
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2024-02-27 21:15:53 +0530

    source upload

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+
+$username = "ramees";
+$password = "ramees";
+$host = "localhost";
+$database = "Plants";
+
+
+$mysqli = new mysqli($host, $username, $password, $database);
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title class=>AyurMeds</title>
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+<body>
+    <nav class="bg-slate-900 text-7xl min-h-[4rem] max-h-[5.6rem]">
+     <section class="title text-6xl text-gray-200  montserrat-600 py-4 text-center">Ayur Meds</section>
+     <section class="admin-login">
+        <a href="login.php"><button class="text-4xl bg-slate-700 rounded-lg p-1 hover:text-slate-300 text-gray-100 relative bottom-[6.5rem] left-[92rem]">Admin Login</button>
+        </a>
+     </section>
+     <div>
+        
+     </div>
+    </nav>
+    
+</body>
+</html>
