f32ba0518dd55334f0560a73b40290d2365d4bcc
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2024-02-27 22:30:11 +0530

    feat: admin login page

---
+++ admin/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin | AyurMeds</title>
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+    
+<nav class=" flex bg-slate-900 text-gray-400  items-center justify-center text-4xl ">
+    <main class="text-gray-200 flex-1">AyurMeds</main> <aside class="text-gray-200 flex-1">Admin Dashboard</aside> 
+</nav>
+    
+</body>
+</html>
