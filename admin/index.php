49ac196e22d4f6d4b463d9731d2dcfa9155ca84f
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2024-02-28 02:30:08 +0530

    feat: add admin view admin view on progress

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,6 @@
+<?php
+include "../connection.php"
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,9 +11,66 @@
 </head>
 <body>
     
-<nav class=" flex bg-slate-900 text-gray-400  items-center justify-center text-4xl ">
-    <main class="text-gray-200 flex-1">AyurMeds</main> <aside class="text-gray-200 flex-1">Admin Dashboard</aside> 
+<nav class=" flex bg-slate-900  text-gray-400   justify-end text-4xl py-3 pb-4 ">
+    <main class="text-gray-200 mr-[7em] text-6xl  montserrat-600 font-bold">AyurMeds</main> <aside class="text-gray-200 self-end bg-slate-700 rounded-lg p-2 ">Admin Dashboard</aside> 
 </nav>
-    
-</body>
+    <section class="content">
+        <section class="add">
+            <h1>Add</h1>
+            <form action="">
+                <select name="plants" id="plants" class="plants">
+                    
+                    <?php
+                    $sql = "select name from plants";
+                    $result = $mysqli->query($sql);
+                    
+                    if(!$result) {
+                        die("Error: ". $mysqli->error);
+                    }
+                    if($result->num_rows > 0){
+                    while($row = $result->fetch_assoc())
+                    {
+                        
+                        $value = $row['name'];
+                        echo "
+                        <option value='$value'>$value</option>
+                        ";
+                    }
+                    }
+                    else {
+                        echo "No plants found";
+                    }
+                    // $mysqli->close();
+                    ?>
+                   
+                    
+                </select>
+                <select name="category" id="" class="category">
+                <?php
+                    $sql = "select type from categories";
+                    $result = $mysqli->query($sql);
+                    
+                    if(!$result) {
+                        die("Error: ". $mysqli->error);
+                    }
+                    if($result->num_rows > 0){
+                    while($row = $result->fetch_assoc())
+                    {
+                        
+                        $value = $row['type'];
+                        echo "
+                        <option value='$value'>$value</option>
+                        ";
+                    }
+                    }
+                    else {
+                        echo "No categories found";
+                    }
+                    // $mysqli->close();
+                    ?>
+                </select>
+            </form>
+        </section>
+    </section>
+</body> 
 </html>
--- index.php
+++ index.php
@@ -5,12 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title class=>AyurMeds</title>
     <link rel="stylesheet" href="./css/styles.css">
+    
 </head>
 <body>
-    <nav class="bg-slate-900 text-7xl min-h-[4rem] max-h-[5.6rem]">
+    <nav class="bg-slate-900 text-7xl min-h-[3.8rem] max-h-[5.3rem]">
      <section class="title text-6xl text-gray-200  montserrat-600 py-4 text-center">Ayur Meds</section>
      <section class="admin-login">
-        <a href="admin/"><button class="text-4xl bg-slate-700 rounded-lg p-1 hover:text-slate-300 text-gray-100 relative bottom-[6.5rem] left-[92rem]">Admin Login</button>
+        <a href="admin/"><button class="text-4xl bg-slate-700 rounded-lg p-2 hover:text-slate-300 text-gray-100 relative bottom-[6.5rem] left-[92rem]">Admin Login</button>
         </a>
      </section>
      <div>
