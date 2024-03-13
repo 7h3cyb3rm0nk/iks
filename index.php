e86ebfdb6441bf91f2311d4de4dac54bd76d15c2
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2024-03-13 15:57:08 +0530

    fix: minor css bug fixes

--- index.php
+++ index.php
@@ -14,19 +14,20 @@ require("connection.php");
 </head>
 <body class="min-h-screen bg-gray-200 ">
     <nav class="bg-gray-500 bg-opacity-80 grid  text-6xl py-5 montserrat-600 transition-all duration-500 grid-cols-10   ">
-    <div class="menu col-span-1 p-3 h-full px-4  cursor-pointer focus:animate-spin "i id="menu">
-        <svg viewBox="0 0 100 80" width="40" height="40" class="stroke-slate-50 mb-5">
-          <rect class="rounded-lg" width="100" height="20"></rect>
-          <rect class="rounded-lg" y="30" width="100" height="20"></rect>
-          <rect class="rounded-lg" y="60" width="100" height="20"></rect>
+    <div class="menu col-span-1 p-3 h-full px-4  cursor-pointer " id="menu">
+        <svg viewBox="0 0 100 80" width="40" height="40" class="stroke-slate-900 mb-5">
+          <rect class="rounded-lg" width="100" height="20" fill="white"></rect>
+          <rect class="rounded-lg" y="30" width="100" height="20" fill="white"></rect>
+          <rect class="rounded-lg" y="60" width="100" height="20" fill="white"></rect>
         </svg>
-        <div class="menu-list rounded-lg text-gray-200 sm:mb-5 translate-x-[-150%] fixed left-0 px-3 pt-8 transition-all duration-500 text-xl min-h-screen bg-slate-700 ml-0 flex items-center ">
-           <div class="list px-12 text-slate-800 py-[10em] rounded-md min-h-fit bg-gray-300 flex gap-10 relative bottom-10 shadow-md">
+        <div class="menu-list rounded-lg text-gray-200 sm:mb-5 translate-x-[-150%] fixed left-0 px-3 pt-8 transition-all duration-500 text-xl min-h-screen ml-0 flex items-center ">
+           <div class="list px-12 text-slate-800 py-[8em] rounded-md min-h-fit bg-gray-300 flex gap-10 relative bottom-10 shadow-md">
             <ul class="grid gap-12">
-                <li class=" hover:text-slate-500 cursor-pointer">ayurmedics</li>
-                <li class=" hover:text-slate-500 cursor-pointer">kalari</li>
-                <li class=" hover:text-slate-500 cursor-pointer">yoga</li>
-                <li class=" hover:text-slate-500 cursor-pointer">music therapy</li>
+                <li class=" hover:text-slate-500 cursor-pointer">Ayurmedics</li>
+                <li class=" hover:text-slate-500 cursor-pointer">Kalari</li>
+                <li class=" hover:text-slate-500 cursor-pointer">Yoga</li>
+                <li class=" hover:text-slate-500 cursor-pointer">Music Therapy</li>
+                <li class="hover:text-slate-500 cursor-pointer">First-Aids</li>
             </ul>
            </div>
        </div>
@@ -50,7 +51,8 @@ require("connection.php");
     let navText = document.querySelector(".navText");
 
     menu.addEventListener("click", () => {
-        menuList.classList.toggle("translate-x-0")
+        menuList.classList.toggle("translate-x-0");
+        menuList.classList.toggle("translate-x-[-150%]");
         menu.classList.toggle("bg-slate-700");
         menu.classList.toggle("fixed");
         menu.classList.toggle("top-0");
