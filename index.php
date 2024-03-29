c8df063945eee051a00e9df8b3f65c2b2b8f8f5e
“Sreeram0045” <sreeramjmenon10@gmail.com>
2024-03-29 16:25:56 +0530

    Add art-forms.php

--- index.php
+++ index.php
@@ -6,15 +6,21 @@ require("connection.php");
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin | AyurMeds</title>
     <link rel="stylesheet" href="css/styles.css?v=<?=time();?>">
-</head>
-<body class="min-h-screen bg-gray-200 ">
-    <?php include __DIR__."/pages/header.php" ?>    
 
-    
-</body> 
+
+
+
+
+<body class="min-h-screen bg-gray-200 ">
+    <?php include __DIR__ . "/pages/header.php" ?>
+
+    <script src="../iks/js/scripts.js"></script>
+</body>
+
 </html>
