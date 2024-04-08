c00ecfec9222b348a833fbd27b1376e1b5abc42d
“Sreeram0045” <sreeramjmenon10@gmail.com>
2024-04-08 11:09:41 +0530

    feat: Updated the artforms.php, added art-forms.css and art-form.js

--- pages/artforms.php
+++ pages/artforms.php
@@ -3,38 +3,167 @@ include("../connection.php");
 ?>
 <!DOCTYPE html>
 <html lang="en" class="scroll-smooth">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IKS | Art Forms</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
     <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/art-forms.css?v=<?= time() ?>">
 </head>
-<body class="bg-gray-50">
 
-<?php
-include("header.php");
-?>
-<section class="snap-y snap-mandatory scroll-smooth scroll-pb-4 ">
-   <section class=" m-5 bg-gray-300 shadow-lg min-h-[80vh] snap-center snap-always  min-w-[80vw] rounded-lg p-4 ">
+<body>
 
-
+    <?php
+    include("header.php");
+    ?>
+    <section>
+        <div class="carousel prev">
+            <div class="list">
+                <div class="item">
+                    <img src="../assets/images-art-forms-jpg/Kathakali.jpg" alt="Kathakali">
+                    <div class="content">
+                        <div class="heading_carousel">Kathakali (16th Century)</div>
+                        <div class="description">
+                            <p>Kathakali, a vibrant classical dance-drama form, emerged in Kerala, India during the
+                                16th century. While its exact origins are debated, scholars believe it developed
+                                from temple rituals and folk art traditions, reaching its fully elaborated style
+                                around this period.</p>
+                            <p>Early performances were likely linked to temple offerings, specifically
+                                Krishnanattam, a dance drama dedicated to the Hindu god Krishna. Over time, themes broadened to encompass stories from the Mahabharata, Ramayana, and other Hindu mythology.</p>
+                        </div>
+                        <form action="" method="get" class="buttons">
+                            <input type="submit" class="bg-transparent text-white border-white border px-4 py-2 rounded-md hover:text-black hover:bg-white active:bg-transparent active:text-black active:bg-white focus:ring-2 focus:ring-slate-500 hover:translate-y-[-4px] active:translate-y-[-5px] hover:scale-105 active:scale-110 transition-all ease-in-out delay-50 tracking-widest" value="SHOW MORE">
+                        </form>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="../assets/images-art-forms-jpg/Mohiniyattam.jpg" alt="Mohiniyattam">
+                    <div class="content">
+                        <div class="heading_carousel">Mohiniyattam (18th Century)</div>
+                        <div class="description">
+                            <p>Mohiniyattam, a gracefully captivating classical dance form, traces its origins to the 18th century in Kerala, India. Known for its elegance and storytelling beauty, Mohiniyattam translates to "dance of the enchantress" - a fitting name for this art form dedicated to the Hindu goddess Mohini, the enchanting avatar of Vishnu.</p>
+                            <p>Legends attribute the creation of Mohiniyattam to the sage Bharata Muni, who documented various dance forms in the ancient Sanskrit text Natya Shastra. While its exact origin story remains debated, Mohiniyattam has flourished in Kerala's temples and courts for centuries. Traditionally, it was a performance art reserved for women from Nair communities. Today, both men and women practice and perform this expressive dance form.</p>
+                        </div>
+                        <form action="" method="get" class="buttons">
+                            <input type="submit" class="bg-transparent text-white border-white border px-4 py-2 rounded-md hover:text-black hover:bg-white active:bg-transparent active:text-black active:bg-white focus:ring-2 focus:ring-slate-500 hover:translate-y-[-4px] active:translate-y-[-5px] hover:scale-105 active:scale-110 transition-all ease-in-out delay-50 tracking-widest" value="SHOW MORE">
+                        </form>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="../assets/images-art-forms-jpg/Padayani.jpg" alt="Padayani">
+                    <div class="content">
+                        <div class="heading_carousel">Padayani (16th Century)</div>
+                        <div class="description">
+                            <p>Padayani, a vibrant ritual art form, is deeply rooted in the central region of Kerala, India. Emerging from temple rituals and folk traditions, Padayani performances are a captivating blend of dance, music, drama, satire, and elaborate masks. This art form is particularly associated with worship of the goddess Bhadrakali, a powerful Hindu deity.</p>
+                            <p>The exact origins of Padayani are debated, but scholars believe it developed sometime around the 16th century. Some theories link it to the rich martial art tradition of Kalaripayattu, while others suggest it emerged from temple rituals dedicated to Bhadrakali. Regardless of its precise beginnings, Padayani has become an integral part of Kerala's cultural identity.</p>
+                        </div>
+                        <form action="" method="get" class="buttons">
+                            <input type="submit" class="bg-transparent text-white border-white border px-4 py-2 rounded-md hover:text-black hover:bg-white active:bg-transparent active:text-black active:bg-white focus:ring-2 focus:ring-slate-500 hover:translate-y-[-4px] active:translate-y-[-5px] hover:scale-105 active:scale-110 transition-all ease-in-out delay-50 tracking-widest" value="SHOW MORE">
+                        </form>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="../assets/images-art-forms-jpg/Koodiyattam.jpg" alt="Koodiyattam">
+                    <div class="content">
+                        <div class="heading_carousel">Koodiyattam (14th-16th Century)</div>
+                        <div class="description">
+                            <p>Koodiyattam, meaning "acting together" in Malayalam, is a remarkable theatre tradition that has endured for centuries in Kerala, India. Believed to be at least 2,000 years old, it holds the title of the oldest surviving Sanskrit theatre form in the world. Koodiyattam offers a captivating glimpse into ancient Indian dramatic traditions and storytelling techniques.</p>
+                            <p>The origins of Koodiyattam are closely linked to temple rituals and performance traditions. Some scholars believe it evolved from Koothu, an older temple art form. Over time, Koodiyattam developed into a highly refined art form with codified acting rules and a distinct performance style.</p>
+                        </div>
+                        <form action="" method="get" class="buttons">
+                            <input type="submit" class="bg-transparent text-white border-white border px-4 py-2 rounded-md hover:text-black hover:bg-white active:bg-transparent active:text-black active:bg-white focus:ring-2 focus:ring-slate-500 hover:translate-y-[-4px] active:translate-y-[-5px] hover:scale-105 active:scale-110 transition-all ease-in-out delay-50 tracking-widest" value="SHOW MORE">
+                        </form>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="../assets/images-art-forms-jpg/Chakyarkoothu.jpg" alt="Chakyarkoothu">
+                    <div class="content">
+                        <div class="heading_carousel">Chakyar Koothu (16th Century)</div>
+                        <div class="description">
+                            <p>Chakyar Koothu, born in 16th-century Kerala, seamlessly merges epics and stand-up comedy, engaging audiences with social commentary. Held in sacred temple spaces, these performances captivate during festivals.</p>
+                            <p>Collaborations between the Chakyar community and Ambalavasi Nambiars further enrich the cultural tapestry of Kerala. Together, they deliver captivating narratives and expressive movements that transcend generations, preserving ancient traditions while resonating with contemporary audiences. </p>
+                        </div>
+                        <form action="" method="get" class="buttons">
+                            <input type="submit" class="bg-transparent text-white border-white border px-4 py-2 rounded-md hover:text-black hover:bg-white active:bg-transparent active:text-black active:bg-white focus:ring-2 focus:ring-slate-500 hover:translate-y-[-4px] active:translate-y-[-5px] hover:scale-105 active:scale-110 transition-all ease-in-out delay-50 tracking-widest" value="SHOW MORE">
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <!-- thumbnail -->
+            <div class="thumbnail">
+                <div class="item">
+                    <img src="../assets/images-art-forms-jpg/Kathakali.jpg">
+                    <div class="content">
+                        <div class="title">
+                            Kathakali
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="../assets/images-art-forms-jpg/Mohiniyattam.jpg">
+                    <div class="content">
+                        <div class="title">
+                            Mohiniyattam
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="../assets/images-art-forms-jpg/Padayani.jpg">
+                    <div class="content">
+                        <div class="title">
+                            Padayani
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="../assets/images-art-forms-jpg/Koodiyattam.jpg">
+                    <div class="content">
+                        <div class="title">
+                            Koodiyattam
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="../assets/images-art-forms-jpg/Chakyarkoothu.jpg">
+                    <div class="content">
+                        <div class="title">
+                            Chakyarkoothu
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="absolute bottom-[15%] left-16 w-[300px] max-w-[30%] flex items-center gap-2 z-20">
+                <button id="prev" class="w-16 h-16 rounded-[50%] bg-[#eee6] text-white hover:bg-white hover:text-black transition-all ease-in-out delay-75 hover:scale-105 active:scale-110">&#60;</button>
+                <button id="next" class="w-16 h-16 rounded-[50%] bg-[#eee6] text-white hover:bg-white hover:text-black transition-all ease-in-out delay-75 hover:scale-105 active:scale-110">&#62;</button>
+            </div>
+        </div>
     </section>
-    <section class=" m-5 bg-gray-300 shadow-lg min-h-[80vh] min-w-[80vw] snap-center snap-always rounded-lg p-4 ">
-    
-
+    <section class="search-section">
+        <form action="" method="get">
+            <div class="searching-container">
+                <span class="search-icon material-symbols-outlined">
+                    search
+                </span>
+                <input type="text" placeholder="Search the Art-form" class="search-input">
+            </div>
+            <input type="submit" class="w-[7rem] ml-4 bg-[#0b64f4] text-white px-4 py-2 rounded-[24px] hover:bg-[#1d4fd7] active:bg-[bg-[#1d4fd7]] hover:translate-y-[-4px] active:translate-y-[-5px] hover:scale-105 active:scale-110 transition-all ease-in-out delay-50 tracking-[2px]" value="SEARCH">
+        </form>
     </section>
-    <section class=" m-5 bg-gray-300 shadow-lg min-h-[80vh] min-w-[80vw] snap-start snap-always   rounded-lg p-4 ">
-    
-
+    <section class="search-result-section">
+        <img src="../assets/images-art-forms-jpg/Kathakali.jpg" alt="Kathakali" class="search-result-img">
+        <div class="result-details">
+            <h2 class="result-heading">Kathakali(16th Century)</h2>
+            <div class="result-content">
+                <p>Kathakali, a vibrant classical dance-drama form, emerged in Kerala, India during the 16th century. While its exact origins are debated, scholars believe it developed from temple rituals and folk art traditions, reaching its fully elaborated style around this period.Early performances were likely linked to temple offerings, specifically Krishnanattam, a dance drama dedicated to the Hindu god Krishna. Over time, themes broadened to encompass stories from the Mahabharata, Ramayana, and other Hindu mythology.Traditionally, Kathakali performances were all-night affairs held in temple courtyards or open-air theatres. Today, they are often shorter, catering to wider audiences. The stage is minimal, with the focus entirely on the actors and the captivating storytelling.</p>
+                <p>The actors, all male, undergo rigorous training in dance, music, and acting. Their elaborate costumes are a sight to behold, featuring colorful headgear, billowing skirt-like garments, and intricate facial makeup that reflects the character's nature - divine, demonic, or heroic.Mudras, expressive hand gestures, and drishti bhedas, captivating eye movements, are central to conveying the narrative. Vocalists provide rhythmic chants (called bhagavadar) that narrate the story, while percussion instruments like the maddalam (drum) and chengala (gong) create a powerful and dramatic soundscape.</p>
+                <p>Kathakali boasts a rich lineage of maestros who have dedicated their lives to preserving and promoting this art form. Masters like Guru Kelu Nair, known for his exceptional footwork, and Vazhenkadu Kunju Nair, a renowned performer and teacher, are just a few examples.Traditionally prevalent among the Nair caste of Kerala, Kathakali has transcended these boundaries and is celebrated throughout the state. It's deeply embedded in Kerala's cultural identity and is a major draw for tourists. Watching a Kathakali performance is a unique window into Kerala's rich heritage and storytelling traditions.</p>
+                <p>Kathakali is not just a performance; it's an immersive experience. Many tourists visit Kerala specifically to witness this art form. Several centers offer workshops where visitors can learn basic mudras and movements, gaining a deeper appreciation for the dedication and artistry involved.Beyond tourism, Kathakali holds immense importance. It's a powerful medium for transmitting cultural values, religious narratives, and ancient stories to new generations. This exquisite art form ensures that Kerala's legacy of dance, drama, and mythology continues to flourish.</p>
+            </div>
+        </div>
     </section>
-    <section class=" m-5 bg-gray-300 shadow-lg min-h-[80vh] min-w-[80vw] snap-start snap-always rounded-lg p-4 ">
-    
-
-    </section>
-    <section class=" m-5 bg-gray-300 shadow-lg min-h-[80vh] min-w-[80vw] snap-start snap-always rounded-lg p-4 ">
-    
-
-    </section>
-</section>
+    <script src="../js/art-form.js"></script>
 </body>
+
 </html>
