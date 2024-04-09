cdd0c9fcf167f214416c02ebf8d70ec11e257f82
“Sreeram0045” <sreeramjmenon10@gmail.com>
2024-04-09 15:16:40 +0530

    feat:Added artformsdescription named php file and added a ajax call to it. ie;created an API

--- pages/artforms.php
+++ pages/artforms.php
@@ -149,6 +149,7 @@
             <input type="submit" class="w-[7rem] ml-4 bg-[#0b64f4] text-white px-4 py-2 rounded-[24px] hover:bg-[#1d4fd7] active:bg-[bg-[#1d4fd7]] hover:translate-y-[-4px] active:translate-y-[-5px] hover:scale-105 active:scale-110 transition-all ease-in-out delay-50 tracking-[2px]" value="SEARCH" name="submit">
         </form>
     </section>
+    <!-- ajax script -->
     <script>
         $(document).ready(function() {
             $('#selectionform').submit(function(event) {
