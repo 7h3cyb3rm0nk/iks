0497de8a59e358568325b6297a25b729364cf6a6
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2024-04-11 12:06:41 +0530

    fix:  styles

--- pages/header.php
+++ pages/header.php
@@ -1,11 +1,11 @@
-<nav class="bg-slate-900 bg-opacity-80 grid  text-6xl py-5 z-10 poppins-700 transition-all duration-500 snap-start snap-proximity grid-cols-10 z-2  ">
+<nav class="bg-slate-900 bg-opacity-80 grid  text-6xl py-5 z-10 poppins-700 transition-all duration-500 snap-start snap-proximity grid-cols-10   ">
     <div class="menu col-span-1 p-3 h-full px-4 cursor-pointer " id="menu">
         <svg viewBox="0 0 100 80" width="40" height="40" class="stroke-slate-900 mb-5">
             <rect class="rounded-lg" width="100" height="20" fill="white"></rect>
             <rect class="rounded-lg" y="30" width="100" height="20" fill="white"></rect>
             <rect class="rounded-lg" y="60" width="100" height="20" fill="white"></rect>
         </svg>
-        <div class="menu-list rounded-lg text-gray-200 sm:mb-5 translate-x-[-150%] fixed left-0 px-3 pt-8 transition-all duration-1000 text-xl min-h-screen ml-0 z-10 py-5 flex justify-center items-center pb-6 ">
+        <div class="menu-list rounded-lg text-gray-200 sm:mb-5 translate-x-[-150%] fixed left-0 px-3 pt-8 transition-all duration-1000 text-xl min-h-screen ml-0 z-50 py-5 flex justify-center items-center pb-6 ">
 
             <div class="list px-12 mb-12 text-gray-200 py-[5em] duration-1000 rounded-md min-h-fit bg-zinc-500 shadow-lg flex gap-10  bottom-10  ">
                 <ul class="grid gap-12 list-disc transition-all duration-300">
