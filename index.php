cebaab5813b3043f3b8461381b48a65c7d039ac3
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2024-04-11 14:41:57 +0530

    feat: add index.php contents

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require("connection.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin | AyurMeds</title>
+    <title>Inidan Knowledge System</title>
     <link rel="stylesheet" href="/css/styles.css?v=<?= time(); ?>">
 
 
@@ -16,7 +16,25 @@ require("connection.php");
 
 <body class="min-h-screen bg-gray-200 ">
     <?php include __DIR__ . "/pages/header.php"; ?>
+    <section class="main-content h-[90vh] pt-[6em] px-12 w-screen" style="background-image: url('assets/jpgs/iks.jpg');">
+        <div class="text-xl poppins-700 w-[70vw] mx-auto text-black  shadow-lg bg-gray-200 bg-opacity-40 p-4 rounded-lg tracking-wide" >
+        <span class="heading-text poppins-600 text-slate-500 text-4xl font-bold w-fit">
+            INDIAN KNOWLEDGE SYSTEM
+        </span>is a comprehensive repository of ancient wisdom and cultural heritage that encapsulates the diverse tapestry of Indian art forms and medicinal plants. Rooted in millennia-old traditions and practices, this knowledge system reflects the rich heritage of India, spanning from the serene rhythms of classical dance to the healing properties of indigenous flora.
+        <p class="mb-4">
+
+Embodied within this system are the intricate movements of Bharatanatyam, Kathak and Kuchipudi, each telling stories of devotion, mythology, and human emotion through graceful gestures and mesmerizing choreography. these art forms serve as conduits of cultural expression, connecting individuals to their heritage and spirituality.
+</p>
+<p class="mb-4">
+Alongside the realm of artistic expression lies the profound wisdom of Ayurveda, India's ancient system of medicine. Within Ayurveda, medicinal plants such as Tulsi, Neem, Ashwagandha, Turmeric, and Amla are revered for their therapeutic properties, offering holistic approaches to health and wellness. From boosting immunity to promoting mental clarity and vitality, these botanical treasures represent the profound connection between nature and human well-being.
+</p>
+<p class="mb-4">
+The Indian Knowledge System stands as a testament to the enduring legacy of India's cultural heritage, serving as a source of inspiration, knowledge, and healing for generations to come. Through its timeless wisdom and vibrant traditions, it continues to enrich lives and foster a deeper understanding of humanity's interconnectedness with the natural world.
+</p>
+        </div>
+    </section>
     <script src="/js/scripts.js"></script>
+    
 </body>
 
 </html>
